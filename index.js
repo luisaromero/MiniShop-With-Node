8b48a20b1a960921e4c252c07ee064d13e21770e
luisaromero <luisa.marcela.romero@gmail.com>
2026-07-16 01:53:57 -0400

    add a new products based in the brand

--- index.js
+++ index.js
@@ -5,6 +5,8 @@ require('dotenv').config();
 
 const express = require('express');
 const path = require('path');
+const products = require('./products');
+
 const { engine } = require('express-handlebars');
 
 const app = express();
@@ -36,68 +38,6 @@ const name_brand = 'Padel Ciaga'
 
 const welcomeMsg = 'Bienvenida a nuestra tienda'
 
-const products = [
-    {
-        nombre: "Camiseta Básica",
-        precio: 15,
-        disponible: true,
-        imagen: "https://images.unsplash.com/photo-1523381294911-8d3cead13475?w=600"
-    },
-    {
-        nombre: "Pantalón Jeans",
-        precio: 30,
-        disponible: false,
-        imagen: "https://images.unsplash.com/photo-1583005008627-cf9c4e1a9d6d?w=600"
-    },
-    {
-        nombre: "Zapatos Deportivos",
-        precio: 50,
-        disponible: true,
-        imagen: "https://images.unsplash.com/photo-1528701800489-20be8c01c1a3?w=600"
-    },
-    {
-        nombre: "Chaqueta de Cuero",
-        precio: 80,
-        disponible: true,
-        imagen: "https://images.unsplash.com/photo-1526170375885-4d8ecf77b99f?w=600"
-    },
-    {
-        nombre: "Gorra Clásica",
-        precio: 12,
-        disponible: true,
-        imagen: "https://images.unsplash.com/photo-1526170375885-bf2f5f0f3e3a?w=600"
-    },
-    {
-        nombre: "Bolso de Mano",
-        precio: 45,
-        disponible: false,
-        imagen: "https://images.unsplash.com/photo-1526170375885-43f5d6d4f00f?w=600"
-    },
-    {
-        nombre: "Reloj Digital",
-        precio: 60,
-        disponible: true,
-        imagen: "https://images.unsplash.com/photo-1526170375885-6c60d6f0f47f?w=600"
-    },
-    {
-        nombre: "Bufanda de Lana",
-        precio: 18,
-        disponible: true,
-        imagen: "https://images.unsplash.com/photo-1526170375885-6b73d6d0f0aa?w=600"
-    },
-    {
-        nombre: "Sudadera Hoodie",
-        precio: 35,
-        disponible: false,
-        imagen: "https://images.unsplash.com/photo-1526170375885-9f25d6f0f077?w=600"
-    },
-    {
-        nombre: "Gafas de Sol",
-        precio: 25,
-        disponible: true,
-        imagen: "https://images.unsplash.com/photo-1526170375885-fc40d6f0f0cc?w=600"
-    }
-];
 
 // ==========================================
 // 5. RUTAS
---
+++ products.js
@@ -0,0 +1,78 @@
+const products = [
+    {
+        nombre: "Conjunto Padel Mujer Naranjo",
+        precio: 45000,
+        disponible: true,
+        imagen: "/img/products/orange-dress.png"
+    },
+    {
+        nombre: "Conjunto Padel Mujer Negro",
+        precio: 45000,
+        disponible: true,
+        imagen: "/img/products/black-dress.png"
+    },
+    {
+        nombre: "Raqueta Rosada",
+        precio: 46000,
+        disponible: true,
+        imagen: "/img/products/pink-racket.png"
+    },
+    {
+        nombre: "Raqueta Multicolor",
+        precio: 40000,
+        disponible: true,
+        imagen: "/img/products/multicolor-racket.png"
+    },
+    {
+        nombre: "Polera Polo Rosada",
+        precio: 20000,
+        disponible: false,
+        imagen: "/img/products/pink-shirt.png"
+    },
+    {
+        nombre: "Raqueta de Padel Pro Aquamarine",
+        precio: 89000,
+        disponible: true,
+        imagen: "/img/products/white-racket.png"
+    },
+    {
+        nombre: "Falda Deportiva Azul",
+        precio: 19000,
+        disponible: false,
+        imagen: "/img/products/blue-skirt.png"
+    },
+    {
+        nombre: "Raqueta de Padel Iniciación",
+        precio: 55000,
+        disponible: true,
+        imagen: "/img/products/black-racket.png"
+    },
+    {
+        nombre: "Conjunto Padel Mujer Rosado",
+        precio: 62000,
+        disponible: true,
+        imagen: "/img/products/pink-set.png"
+    },
+
+    {
+        nombre: "Botella de Agua Rosada 750ml",
+        precio: 9000,
+        disponible: true,
+        imagen: "/img/products/pink-water-bottle.png"
+    },
+    {
+        nombre: "Jockey Deportiva azul",
+        precio: 15000,
+        disponible: true,
+        imagen: "/img/products/blue-jockey.png"
+    },
+    {
+        nombre: "Tubo de Pelotas Padel x3",
+        precio: 7500,
+        disponible: true,
+        imagen: "/img/products/balls.png"
+    },
+
+];
+
+module.exports = products;
